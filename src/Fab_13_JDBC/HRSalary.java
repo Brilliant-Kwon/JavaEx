2fb4c2bb314b197dd94b92a4c04a74676ba7b417
BRooT Kwon <k1212keun@skuniv.ac.kr>
2019-02-13 11:40:28 +0900

    ver 1.00

--- src/Fab_13_JDBC/HRSalary.java
+++ src/Fab_13_JDBC/HRSalary.java
@@ -3,7 +3,7 @@ package Fab_13_JDBC;
 import java.sql.*;
 import java.util.Scanner;
 
-public class HRSearchEmployees {
+public class HRSalary {
     public static void main(String[] args) {
         String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
         Connection conn = null;
@@ -11,8 +11,18 @@ public class HRSearchEmployees {
         ResultSet rs = null;
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("검색할 이름 또는 철자 : ");
-        String name = sc.nextLine();
+        System.out.print("최소급여, 최대급여 입력(공백으로 구분하세요.) : ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        if (num1 > num2) {
+            int temp;
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        String min = Integer.toString(num1);
+        String max = Integer.toString(num2);
 
         try {
             //2.드라이버로드
@@ -23,24 +33,21 @@ public class HRSearchEmployees {
             System.out.println("연결 성공!");
             //4.Statement 생성
             stmt = conn.createStatement();
-            String sql = "SELECT first_name || ' ' || last_name as name, email, phone_number, hire_date " +
-                    "FROM employees WHERE first_name || ' ' || last_name LIKE '%" + name + "%'";
+            String sql = "SELECT first_name || ' ' || last_name as name, salary " +
+                    "FROM employees WHERE salary <= " + max + " AND salary >= " + min;
             System.out.println("SQL : " + sql);
 
             //5.결과값을 얻기 -> ResultSet
             rs = stmt.executeQuery(sql);
             System.out.println("\n 검색결과 출력\n=========================================================================================");
-            System.out.printf("%-25s %-15s %-20s %-30s\n", "name", "email", "phone_number", "hire_date");
+            System.out.printf("%-25s %-10s\n", "name", "salary");
 //            System.out.println("name,   email,  phone_number,   hire_date");
             System.out.println("=========================================================================================");
             while (rs.next()) {
                 String emp0 = rs.getString("name");     //이름 + 성
-//                String emp1 = rs.getString("last_name");
-                String emp2 = rs.getString("email");    //이메일
-                String emp3 = rs.getString("phone_number");//전화번호
-                String emp4 = rs.getString("hire_date");//고용일
+                String emp1 = rs.getString("salary");
 
-                System.out.printf("%-25s %-15s %-20s %-30s\n", emp0, emp2, emp3, emp4);
+                System.out.printf("%-25s %-10s\n", emp0, emp1);
             }
         } catch (ClassNotFoundException e) {
             System.err.println("JDBC 드라이버를 찾지 못하였습니다..");
